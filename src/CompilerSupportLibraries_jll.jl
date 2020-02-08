c10721709ac3a94cfb01d3cd1177c72106dbf703
Elliot Saba <staticfloat@gmail.com>
2020-02-07 16:42:46 -0800

    CompilerSupportLibraries_jll build 0.2.2+0

--- src/CompilerSupportLibraries_jll.jl
+++ src/CompilerSupportLibraries_jll.jl
@@ -1,4 +1,23 @@
 module CompilerSupportLibraries_jll
+
+if VERSION < v"1.3.0-rc4"
+    # We lie a bit in the registry that JLL packages are usable on Julia 1.0-1.2.
+    # This is to allow packages that might want to support Julia 1.0 to get the
+    # benefits of a JLL package on 1.3 (requiring them to declare a dependence on
+    # this JLL package in their Project.toml) but engage in heroic hacks to do
+    # something other than actually use a JLL package on 1.0-1.2.  By allowing
+    # this package to be installed (but not loaded) on 1.0-1.2, we enable users
+    # to avoid splitting their package versions into pre-1.3 and post-1.3 branches
+    # if they are willing to engage in the kinds of hoop-jumping they might need
+    # to in order to install binaries in a JLL-compatible way on 1.0-1.2. One
+    # example of this hoop-jumping being to express a dependency on this JLL
+    # package, then import it wtihin a `VERSION >= v"1.3"` conditional, and use
+    # the deprecated `build.jl` mechanism to download the binaries through e.g.
+    # `BinaryProvider.jl`.  This should work well for the simplest packages, and
+    # require greater and greater heroics for more and more complex packages.
+    error("Unable to import CompilerSupportLibraries_jll on Julia versions older than 1.3!")
+end
+
 using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
 import Base: UUID
 
@@ -11,7 +30,7 @@ const LIBPATH_list = String[]
 artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
 
 # Extract all platforms
-artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("e66e0078-7015-5450-92f7-15fbd957f2ae"))
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("2a0299ec-ce93-5294-a8a9-ec9401735631"))
 platforms = [Pkg.Artifacts.unpack_platform(e, "CompilerSupportLibraries", artifacts_toml) for e in artifacts["CompilerSupportLibraries"]]
 
 # Filter platforms based on what wrappers we've generated on-disk
