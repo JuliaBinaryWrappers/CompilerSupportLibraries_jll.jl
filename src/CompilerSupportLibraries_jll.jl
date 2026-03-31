8b4fd9e0f1b766fec1c25f47754ee5f3adcc0565
jlbuild <juliabuildbot@gmail.com>
2026-03-31 11:56:31 +0000

    CompilerSupportLibraries_jll build 1.4.0+0

--- src/CompilerSupportLibraries_jll.jl
+++ src/CompilerSupportLibraries_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("CompilerSupportLibraries")
-JLLWrappers.@generate_main_file("CompilerSupportLibraries", UUID("e66e0078-7015-5450-92f7-15fbd957f2ae"))
+JLLWrappers.@generate_main_file("CompilerSupportLibraries", Base.UUID("e66e0078-7015-5450-92f7-15fbd957f2ae"))
 end  # module CompilerSupportLibraries_jll
